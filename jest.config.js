8ca25e1301721ae189af78af709aa2f7dc51d597
Hoya <visionsky10@gmail.com>
2026-07-21 15:38:16 +0900

    PR 자동 검증과 Jest 설정 보완

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: [
+    'react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/jest.setup.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native.*|@react-native(-community)?|@react-navigation)/)',
+  ],
 };
